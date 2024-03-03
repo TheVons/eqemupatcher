c7391b6d10c8296bde1a767433603e82efddda68
Vons <26316029+TheVons@users.noreply.github.com>
2024-03-03 10:31:29 -0500

    Update script

--- Update-Files.ps1
+++ Update-Files.ps1
@@ -3,6 +3,6 @@ param(
 )
 
 Invoke-WebRequest -Uri https://spire.cityraiders.net/api/v1/eqemuserver/export-client-file/spells -OutFile "$baseDir\rof\spells_us.txt"
-Invoke-WebRequest -Uri https://spire.cityraiders.net/api/v1/eqemuserver/export-client-file/skills -OutFile "$baseDir\rof\SkillCaps.txt"
-Invoke-WebRequest -Uri https://spire.cityraiders.net/api/v1/eqemuserver/export-client-file/basedata -OutFile "$baseDir\rof\BaseData.txt"
+Invoke-WebRequest -Uri https://spire.cityraiders.net/api/v1/eqemuserver/export-client-file/skills -OutFile "$baseDir\rof\Resources\SkillCaps.txt"
+Invoke-WebRequest -Uri https://spire.cityraiders.net/api/v1/eqemuserver/export-client-file/basedata -OutFile "$baseDir\rof\Resources\BaseData.txt"
 Invoke-WebRequest -Uri https://spire.cityraiders.net/api/v1/eqemuserver/export-client-file/dbstring -OutFile "$baseDir\rof\dbstr_us.txt"
